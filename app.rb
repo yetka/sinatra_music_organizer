fa0a4420e290892123ba6d93b23a3e21aa9f4515
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-11-30 15:39:42 -0800

    add views and app file to connec with lib

---
+++ app.rb
@@ -0,0 +1,25 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/music_organizer')
+require('pry')
+
+get('/') do
+  @list = Artist.all()
+  erb(:list)
+end
+
+post('/output') do
+  name = params["name"]
+  genre = params["genre"]
+
+  artist = Artist.new({:name=> name, :genre=> genre})
+  artist.save()
+  @list = Artist.all()
+  erb(:list)
+end
+
+get('/artist/:id') do
+  @artist = Artist.find(params[:id])
+  erb(:artist)
+end
